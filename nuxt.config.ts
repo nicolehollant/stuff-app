e54d6bf9c92b60c653081f0c60aba503546d3dde
Nicole Hollant <nicolehollant@gmail.com>
2023-11-25 00:21:15 -0500

    ugh

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ export default defineNuxtConfig({
   supabase: {
     redirect: false,
   },
+  runtimeConfig: {
+    public: {
+      SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+      VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL,
+    },
+  },
 
   modules: ["senp-ui", "@nuxtjs/supabase"],
   build: {
